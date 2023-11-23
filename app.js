180bd39d95ee4690ce3856fb596acedb84a48ccf
jornkri <jorn.kristiansen@geodata.no>
2023-11-23 10:59:07 +0100

    fix

--- app.js
+++ app.js
@@ -3,25 +3,26 @@ import { Gantt } from './gantt.module.js';
 
 //const gantt = new Gantt({/*...*/ });
 
-async function getBolts(url="https://services-eu1.arcgis.com/eePcGuRGPyGzmI0A/arcgis/rest/services/Basrapport%20Aktivitet/FeatureServer/0/query", data={}){
+async function getActivity(url="https://services-eu1.arcgis.com/eePcGuRGPyGzmI0A/arcgis/rest/services/Basrapport%20Aktivitet/FeatureServer/0/query", data={}){
     const params = new URLSearchParams({
         returnGeometry: "false",
-        where: data.where, //"aktivitet = '1 Bolter'",
+        where: data.where,//"aktivitet = '1 Bolter'",
         outFields: "fra_tidspunkt, til_tidspunkt, aktivitet, antall, OBJECTID",
         f: "json"
     });
 
     const response = await fetch(`${url}?${params}`, {
         method: "GET",
-        // mode: "cors",
-        // headers:{
-        //     "Content-Type": "application/json"
-        // },
+        mode: "cors",
+        headers:{
+            "Content-Type": "application/json"
+        },
 
     })
 
     return response.json();
 }
+
 
 const sBetongData = await getActivity("https://services-eu1.arcgis.com/eePcGuRGPyGzmI0A/arcgis/rest/services/Basrapport%20Aktivitet/FeatureServer/0/query", {where: "aktivitet = '1 Sprøytebetong'"});
 const sBetongKids = sBetongData.features.map(ft => ({id: ft.attributes.OBJECTID, name:ft.attributes.aktivitet, startDate: new Date(ft.attributes.fra_tidspunkt), endDate: new Date(ft.attributes.til_tidspunkt)}))

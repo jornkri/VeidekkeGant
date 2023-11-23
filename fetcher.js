18dd085377bc26ff81563d716a34e6dbd60c1f2c
jornkri <jorn.kristiansen@geodata.no>
2023-11-23 10:25:52 +0100

    fetch arcgis data

--- fetcher.js
+++ fetcher.js
@@ -1,8 +1,3 @@
-
-import { Gantt } from './gantt.module.js';
-
-//const gantt = new Gantt({/*...*/ });
-
 async function getBolts(url="https://services-eu1.arcgis.com/eePcGuRGPyGzmI0A/arcgis/rest/services/Basrapport%20Aktivitet/FeatureServer/0/query", data={}){
     const params = new URLSearchParams({
         returnGeometry: "false",
@@ -26,29 +21,4 @@ async function getBolts(url="https://services-eu1.arcgis.com/eePcGuRGPyGzmI0A/ar
 const feats = await getBolts();
 const kids = feats.features.map(ft => ({id: ft.attributes.OBJECTID, name:ft.attributes.aktivitet, startDate: new Date(ft.attributes.fra_tidspunkt), endDate: new Date(ft.attributes.til_tidspunkt)}))
 
-const gantt = new Gantt({
-    appendTo : document.body,
-
-    startDate : new Date(2022, 0, 1),
-    endDate   : Date.now(),
-
-    project : {
-
-        tasksData : [
-            {
-                id       : 1,
-                name     : 'Bolter',
-                expanded : true,
-                children : kids
-            }
-        ],
-
-        // dependenciesData : [
-        //     { fromTask : 2, toTask : 3 }
-        // ]
-    },
-
-    columns : [
-        { type : 'name', width : 160 }
-    ]
-});
+console.log(kids)

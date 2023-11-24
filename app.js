f434ad143ba22a8d2a20b7364618597afc7b398a
jornkri <jorn.kristiansen@geodata.no>
2023-11-24 08:40:57 +0100

    nyTjeneste

--- app.js
+++ app.js
@@ -3,34 +3,11 @@ import { Gantt } from './gantt.module.js';
 
 //const gantt = new Gantt({/*...*/ });
 
-// async function getActivity(url="https://services-eu1.arcgis.com/eePcGuRGPyGzmI0A/arcgis/rest/services/Basrapport%20Aktivitet/FeatureServer/0/query", data={}){
-//     const params = new URLSearchParams({
-//         returnGeometry: "false",
-//         where: data.where,//"aktivitet = '1 Bolter'",
-//         outFields: "fra_tidspunkt, til_tidspunkt, aktivitet, antall, aktivet_label, OBJECTID" ,
-//         f: "json"
-//     });
-
-//     const response = await fetch(`${url}?${params}`, {
-//         method: "GET"
-//     })
-
-//     return response.json();
-// }
-
-
-// const sBetongData = await getActivity("https://services-eu1.arcgis.com/eePcGuRGPyGzmI0A/arcgis/rest/services/Basrapport%20Aktivitet/FeatureServer/0/query", {where: "aktivitet = '1 Sprøytebetong' AND fra_tidspunkt < til_tidspunkt"});
-// const sBetongKids = sBetongData.features.map(ft => ({id: ft.attributes.OBJECTID, name:ft.attributes.aktivet_label, startDate: new Date(ft.attributes.fra_tidspunkt), endDate: new Date(ft.attributes.til_tidspunkt)}))
-
-// const bolt1Data = await getActivity("https://services-eu1.arcgis.com/eePcGuRGPyGzmI0A/arcgis/rest/services/Basrapport%20Aktivitet/FeatureServer/0/query", {where: "aktivitet = '1 Bolter' AND fra_tidspunkt < til_tidspunkt"});
-// const bolt1Kids = bolt1Data.features.map(ft => ({id: ft.attributes.OBJECTID+1, name:ft.attributes.aktivet_label, startDate: new Date(ft.attributes.fra_tidspunkt), endDate: new Date(ft.attributes.til_tidspunkt)}))
-
-
-async function getActivity(url="https://services.arcgis.com/2JyTvMWQSnM2Vi8q/arcgis/rest/services/testdata_Gantdiagram/FeatureServer/0/query", data={}){
+async function getActivity(url="https://veidekke.cloudgis.no/enterprise/rest/services/Hosted/Basrapport/FeatureServer/0/query", data={}){
     const params = new URLSearchParams({
         returnGeometry: "false",
         where: data.where,//"aktivitet = '1 Bolter'",
-        outFields: "fra_tidspunkt, til_tidspunkt, aktivitet, antall, aktivet_label, OBJECTID_1" ,
+        outFields: "fra_klokken, til_klokken, aktivitet, varighet , aktivet_label, objectid" ,
         f: "json"
     });
 
@@ -42,34 +19,59 @@ async function getActivity(url="https://services.arcgis.com/2JyTvMWQSnM2Vi8q/arc
 }
 
 
-const sBetongData = await getActivity("https://services.arcgis.com/2JyTvMWQSnM2Vi8q/arcgis/rest/services/testdata_Gantdiagram/FeatureServer/0/query", {where: "aktivitet = 'Sprøytebetong' AND fra_tidspunkt < til_tidspunkt"});
-const sBetongKids = sBetongData.features.map(ft => ({id: ft.attributes.OBJECTID_1, name:ft.attributes.aktivitet, startDate: new Date(ft.attributes.fra_tidspunkt), endDate: new Date(ft.attributes.til_tidspunkt), manuallyScheduled: true}))
+const boltmonteringData = await getActivity("https://veidekke.cloudgis.no/enterprise/rest/services/Hosted/Basrapport/FeatureServer/0/query", {where: "aktivitiet = 'Boltemontering' AND fra_klokken < til_tidspunkt"});
+const boltmonteringKids = boltmonteringData.features.map(ft => ({id: ft.attributes.objectid, name:ft.attributes.aktivitet, startDate: new Date(ft.attributes.fra_klokken), endDate: new Date(ft.attributes.til_klokken), manuallyScheduled: true}))
 
-const bolt1Data = await getActivity("https://services.arcgis.com/2JyTvMWQSnM2Vi8q/arcgis/rest/services/testdata_Gantdiagram/FeatureServer/0/query", {where: "aktivitet = 'Bolt' AND fra_tidspunkt < til_tidspunkt"});
-const bolt1Kids = bolt1Data.features.map(ft => ({id: ft.attributes.OBJECTID_1, name:ft.attributes.aktivitet, startDate: new Date(ft.attributes.fra_tidspunkt), endDate: new Date(ft.attributes.til_tidspunkt), manuallyScheduled: true}))
+const boringData = await getActivity("https://veidekke.cloudgis.no/enterprise/rest/services/Hosted/Basrapport/FeatureServer/0/query", {where: "aktivitiet = 'Boring' AND fra_tidspunkt < til_tidspunkt"});
+const boringKids = boringData.features.map(ft => ({id: ft.attributes.objectid, name:ft.attributes.aktivitet, startDate: new Date(ft.attributes.fra_klokken), endDate: new Date(ft.attributes.til_klokken), manuallyScheduled: true}))
 
 
-console.log(bolt1Kids)
-console.log(sBetongKids)
+// async function getActivity(url="https://services.arcgis.com/2JyTvMWQSnM2Vi8q/arcgis/rest/services/testdata_Gantdiagram/FeatureServer/0/query", data={}){
+//     const params = new URLSearchParams({
+//         returnGeometry: "false",
+//         where: data.where,//"aktivitet = '1 Bolter'",
+//         outFields: "fra_tidspunkt, til_tidspunkt, aktivitet, antall, aktivet_label, OBJECTID_1" ,
+//         f: "json"
+//     });
+
+//     const response = await fetch(`${url}?${params}`, {
+//         method: "GET"
+//     })
+
+//     return response.json();
+// }
+
+
+// const sBetongData = await getActivity("https://services.arcgis.com/2JyTvMWQSnM2Vi8q/arcgis/rest/services/testdata_Gantdiagram/FeatureServer/0/query", {where: "aktivitet = 'Sprøytebetong' AND fra_tidspunkt < til_tidspunkt"});
+// const sBetongKids = sBetongData.features.map(ft => ({id: ft.attributes.OBJECTID_1, name:ft.attributes.aktivitet, startDate: new Date(ft.attributes.fra_tidspunkt), endDate: new Date(ft.attributes.til_tidspunkt), manuallyScheduled: true}))
+
+// const bolt1Data = await getActivity("https://services.arcgis.com/2JyTvMWQSnM2Vi8q/arcgis/rest/services/testdata_Gantdiagram/FeatureServer/0/query", {where: "aktivitet = 'Bolt' AND fra_tidspunkt < til_tidspunkt"});
+// const bolt1Kids = bolt1Data.features.map(ft => ({id: ft.attributes.OBJECTID_1, name:ft.attributes.aktivitet, startDate: new Date(ft.attributes.fra_tidspunkt), endDate: new Date(ft.attributes.til_tidspunkt), manuallyScheduled: true, eventColor : 'violet'}))
+
+
+console.log(boltmonteringKids)
+console.log(boringKids)
 const gantt = new Gantt({
     appendTo : document.body,
 
-    startDate : new Date(2023, 10, 1),
+    startDate : new Date(2022, 10, 1),
     endDate   : Date.now(),
     project : {
 
         tasksData : [
             {
-                id       : 'yoyo',
+                id       : 'Sproytebetong',
                 name     : 'Sprøytebetong',
                 expanded : true,
-                children : sBetongKids
+                startDate: new Date(2022, 10, 1),
+                segments   : boltmonteringKids
             },
             {
-                id       : 'hei',
-                name     : '1 Bolter',
+                id       : 'boring',
+                name     : 'Boring',
                 expanded : true,
-                children : bolt1Kids
+                startDate: new Date(2022, 10, 1),
+                segments : boringKids
             }
         ],
 
